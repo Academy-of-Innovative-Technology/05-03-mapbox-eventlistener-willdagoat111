e2a7764c5ec2adbcb5e8bcb8e61692156a7b328d
“William <wihezuo26@aoiths.org>
2026-04-28 13:59:48 -0400

    updated data

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,25 @@
 
-mapboxgl.accessToken = ' ACCESS TOKEN ';
-
+mapboxgl.accessToken = config.MAPBOX_API;
 
 var mapProps = {
-	container: 'map',
-	center: [-73.97, 40.75],
-	zoom: 9
+    container: 'map',
+    center: [-73.97, 40.75],
+    zoom: 9
 };
 
 var map = new mapboxgl.Map(mapProps);
 
-map.on(" event ", function(evt){
+let counter = 0;
 
-	
-	
+const inputBox = document.getElementById("clickCount");
+
+map.on("click", function (evt) {
+    counter += 1;
+    inputBox.value = counter;
 });
+
+map.on("dblclick", function (evt) {
+    counter += 4;
+    inputBox.value = counter;
+});
+map.doubleClickZoom.disable();
